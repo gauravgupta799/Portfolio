e156af72a7357a63ce46a5c88c8c991c590e8ddc
Gaurav Gupta <86770283+gauravgupta799@users.noreply.github.com>
2022-07-20 23:47:19 +0530

    addedMorePages

--- MyPortfolio/script.js
+++ MyPortfolio/script.js
@@ -7,11 +7,45 @@ $(document).ready(function() {
         }else{
             $('.navbar').removeClass('sticky');
         }
+        if(this.scroll > 500){
+            $('.scroll-up-btn').addClass('show');
+        }else{
+            $('.scroll-up-btn').removeClass('show');
+        }
+
+        // slide-up-script
+        $('.scroll-up-btn').click(function() {
+            // console.log("Slide Up");
+            $('html').animate({scrollTop: 0});
+        });
     });
     // toggle
     $('.menu-btn').click(function() {
         $('.navbar .menu').toggleClass('active');
         $('.menu-btn i').toggleClass('active');
         // console.log("Toggle active");
+    });
+
+    // owl carousel
+    $('.carousel').owlCarousel({
+      margin:20,
+      loop:true,
+      autoplay:true,
+      autoplayTimeOut:2000,
+      autoplayHoverPause:true,
+      responsive: {
+        0:{
+            items:1,
+            nav:false,
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:false
+        }
+      }
     });
 })
